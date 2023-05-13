74b8f2446a61bfc16e74e5648c45bd2ac334fc2e
Sergio-FilhoIF <125199037+Sergio-FilhoIF@users.noreply.github.com>
2023-05-13 13:51:31 -0300

    alteração

--- dicionnario/conteudo.py
+++ dicionnario/conteudo.py
@@ -21,5 +21,26 @@
 # else:
 #     print("Algo de errado.")
 #===========================================
-#
+#Acessar dicionario dentro de outro
+# livros = {
+# "livro1": {
+# "titulo": "A Arte da Guerra",
+# "autor": "Sun Tzu",
+# "ano": 500
+# },
+# "livro2": {
+# "titulo": "O Senhor dos Anéis",
+# "autor": "J.R.R. Tolkien",
+# "ano": 1954
+# }
+# }
+
+# titulo_do_livro1 = livros["livro1"]["ano"]
+# print(titulo_do_livro1)
+#===========================================
+#Criar um dicionario aninhado com informações
+# for livro, detalhes in livros.items():
+#     print(f"Titulo do livro {livro}: {detalhes['titulo']}")
+#===========================================
+
 #===========================================
---
+++ dicionnario/questao5.1.py
@@ -0,0 +1,6 @@
+# Crie um dicionário em Python com as seguintes chaves e valores:
+dicionario = {
+    "nome": "João da Silva",
+    "idade":30,
+    "cidade": "maranguape"
+}
