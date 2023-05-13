9b509bb734d1de37acaa2cf3d4f40375b0b8ff1b
Sergio-FilhoIF <125199037+Sergio-FilhoIF@users.noreply.github.com>
2023-05-13 13:54:05 -0300

    alter

---
+++ dicionnario/questao5.2.py
@@ -0,0 +1,9 @@
+# Crie um dicionário em Python que represente uma lista de compras. As chaves do dicionário devem ser os
+# nomes dos produtos e os valores devem ser as quantidades desses produtos a serem compradas.
+dicionario = {
+    'Arroz':2,
+    'feijão':1,
+    'óleo':1,
+    'açúcar':1,
+    'sal':1
+}
